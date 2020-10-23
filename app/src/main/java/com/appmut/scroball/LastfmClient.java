5296d51ad1211cb62f45a047755a41438216e246
kgit42 <49207690+kgit42@users.noreply.github.com>
2020-10-23 16:53:19 +0200

    fix

--- app/src/main/java/com/appmut/scroball/LastfmClient.java
+++ app/src/main/java/com/appmut/scroball/LastfmClient.java
@@ -561,8 +561,8 @@ public class LastfmClient {
               }else{
                 ScrobbleIdentifier scrobbleIdentifier = new ScrobbleIdentifier(result.getTrack(), result.getArtist(), result.getTimestamp());
                 //String scrobbleString = result.getTrack() + result.getArtist() + result.getTimestamp(); //Kai
-                if((!scrobbleIdentifier.getTrack().equals("null") && !scrobbleIdentifier.getArtist().equals("null")) && (!scrobbleIdentifier.getTrack().equals("\"") && !scrobbleIdentifier.getArtist().equals("\"")) && !(scrobbleIdentifier.getTrack().equals("") || scrobbleIdentifier.getTrack().equals(""))){ //&& !lastScrobbledTracks.contains(trackAndArtist)){  //Kai
-                  lastScrobbledTracks.add(new ScrobbleIdentifier(result.getTrack(), result.getArtist(), result.getTimestamp()));  //Kai
+                if((!scrobbleIdentifier.getTrack().equals("null") && !scrobbleIdentifier.getArtist().equals("null")) && (!scrobbleIdentifier.getTrack().equals("\"") && !scrobbleIdentifier.getArtist().equals("\"")) && !(scrobbleIdentifier.getTrack().equals("") || scrobbleIdentifier.getArtist().equals(""))){ //&& !lastScrobbledTracks.contains(trackAndArtist)){  //Kai
+                  lastScrobbledTracks.add(scrobbleIdentifier);  //Kai
                   lastXScrobbledTracks.add(result.getTrack() + result.getArtist()); //KAI
                 }
 
